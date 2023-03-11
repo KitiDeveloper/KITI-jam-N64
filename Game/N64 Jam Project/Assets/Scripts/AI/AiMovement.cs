73198555fb0b9fc6ffa2a5dcbf1a4c48e084f16c
sisimonis <39210425+sisimonis@users.noreply.github.com>
2023-03-11 11:20:20 +0100

    ai simple movement

--- Game/N64 Jam Project/Assets/Scripts/AI/AiMovement.cs
+++ Game/N64 Jam Project/Assets/Scripts/AI/AiMovement.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AiMovement : MonoBehaviour
 {
+
+    private NavMeshAgent agent;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(player.transform.position);
     }
 }
