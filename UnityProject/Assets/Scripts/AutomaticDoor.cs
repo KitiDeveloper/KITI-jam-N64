91f69a60df23f71fd8751b35dd334605bfbcda41
KitiDev <kitiexecutiveoffices@gmail.com>
2023-04-21 17:32:59 +0100

    made an edit to the automatic door script to set proximity

--- UnityProject/Assets/Scripts/AutomaticDoor.cs
+++ UnityProject/Assets/Scripts/AutomaticDoor.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class AutomaticDoor : MonoBehaviour
 {
-    public float liftDistance;          // How far to lift the door
-    public float liftSpeed;             // How quickly to lift the door
+    public float liftDistance = 2f;          // How far to lift the door
+    public float proximityDistance = 4f;
+    public float liftSpeed = 2f;             // How quickly to lift the door
     public Transform doorTransform;     // Reference to the door's transform component
     public Transform playerTransform;   // Reference to the player's transform component
     private Vector3 originalPosition;   // Original position of the door
@@ -18,13 +19,13 @@ public class AutomaticDoor : MonoBehaviour
         isLifting = false;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (Vector3.Distance(doorTransform.position, playerTransform.position) < liftDistance && !isLifting)
+        if (Vector3.Distance(doorTransform.position, playerTransform.position) < proximityDistance && !isLifting)
         {
             StartCoroutine(LiftDoor());
         }
-        else if (Vector3.Distance(doorTransform.position, playerTransform.position) > liftDistance && isLifting && !isPlayerInside)
+        else if (Vector3.Distance(doorTransform.position, playerTransform.position) > proximityDistance && isLifting && !isPlayerInside)
         {
             StartCoroutine(LowerDoor());
         }
